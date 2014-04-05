0cf170227427656cf9c67e1b8bbf98b9b81175ae
tuwilof <tuwilof@gmail.com>
2014-04-05 22:08:06 +0400

    first files

---
+++ action.php
@@ -0,0 +1,2 @@
+Здравствуйте, <?php echo $_POST['login']; ?>.
+Ваш пароль <?php echo $_POST['password']; ?> .
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title>Eat small piece</title>
+	</head>
+	<body>
+	<form action="action.php" method="post">
+		<input type="text" name="login"><br />
+		<input type="password" name="password"><br />
+		<input type=submit>
+	</form>
+	</body>
+</html>
