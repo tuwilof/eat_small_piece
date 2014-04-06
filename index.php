b34199fbb11aed6a215db7d010e82e79306877ed
tuwilof <tuwilof@gmail.com>
2014-04-06 12:12:52 +0400

    changed login form

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 	<head>
 		<meta charset=utf-8>
 		<title>Eat small piece</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-	<form action="action.php" method="post">
-		<input type="text" name="login"><br />
-		<input type="password" name="password"><br />
-		<input type=submit>
-	</form>
+	<div id="main">
+		<div id="logo">
+			<img src="logo.png">
+			<div id="textlogo">Eat small piece</div>
+		</div>
+		<form action="action.php" method="post">
+			<input type="text" name="login" id="login"  value="username"><br />
+			<input type="password" name="password" id="pass"  value="password"><br />
+			<input type=submit id="subm">
+		</form>
+	</div>
 	</body>
 </html>
