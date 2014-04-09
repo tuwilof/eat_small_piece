875f64b39b8ad537d08ecad868f667c2639c41e5
tuwilof <tuwilof@gmail.com>
2014-04-09 22:59:09 +0400

    output work, input not work

--- piece.php
+++ piece.php
@@ -12,17 +12,30 @@
 		$id = $_GET['id'];
 		$result = mysql_query("SELECT * FROM task WHERE id ='$id'");
 		$item = mysql_fetch_array($result);
+		$string = $item['subtasks'];
+		$array = unserialize( $string );
 	?>
 		<?=$item['title']?><br />
-		<form method='post' action='../addpiece.php?id=<?=$item['id']?>'>
-			1 <input type='text' name='title1'><br />
-			2 <input type='text' name='title2'><br />
-			3 <input type='text' name='title3'><br />
-			4 <input type='text' name='title4'><br />
+		<script>var n=5</script>
+		<form name='dim' method='post' action='../addpiece.php?id=<?=$item['id']?>&n=4'>
+			<div id="divf">
+			1 <input type='text' name='title1' value='<?=$array[0]?>'><br />
+			2 <input type='text' name='title2' value='<?=$array[1]?>'><br />
+			3 <input type='text' name='title3' value='<?=$array[2]?>'><br />
+			4 <input type='text' name='title4' value='<?=$array[3]?>'><br />
+			</div>
+			<input type=button onclick=plus()><br />
 			<input type="submit" value="Добавить"><br />
 		</form>
 	<?php
 	}
 ?>
+<script>
+function plus(){
+document.getElementById('divf').innerHTML+=n+' <input type=text name="title'+n+'" value="<?=$array['+n+']?>"><br />'
+n++
+document.dim.action='../addpiece.php?id=<?=$item['id']?>&n='+n
+}
+</script>
 	</body>
 </html>
