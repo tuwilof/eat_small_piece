c9417642eb128d8b3150e2ee4ba589d40c94b020
tuwilof <tuwilof@gmail.com>
2014-04-14 21:17:07 +0400

    repaired

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 	$page = trim($_GET['page']);
+	echo $page;
 	if ($page == "pies") {
 		require_once("engine/model.php");
 		$data = methodName();
 		require_once("template/tasklist.tpl");
 	}
-	else if ($page = "add") {
+	else if ($page == "add") {
 		require_once("template/add.tpl");
 	}
-	else if ($page = "piece") {
+	else if ($page == "piece") {
 		require_once("template/piece.tpl");
 	}
 	else {
