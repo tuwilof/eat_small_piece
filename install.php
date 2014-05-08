ba5e396ca05212d00cbdd4d237df05de16fe55fa
tuwilof <tuwilof@gmail.com>
2014-05-08 14:18:29 +0400

    changed interface form page add task

--- install.php
+++ install.php
@@ -1,6 +1,22 @@
-<?php
-	require_once("template/header.tpl");
-?>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $titlePage; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="../template/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../template/css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
 <div class="container">
 	<div class="page-header">
 		<h1>Install</h1>
--- template/add.tpl
+++ template/add.tpl
@@ -1,6 +1,15 @@
 <div class="container">
-	<form method='post' action='../engine/addtask.php'>
-		<p>Название задачи: <input type='text' name='title'></p>
-		<p><input type="submit" value="Добавить"></p>
+	<form method='post' action='../engine/addtask.php' class="form-horizontal" role="form">
+		<div class="form-group">
+			<label class="col-sm-2 control-label">Title task</label>
+			<div class="col-sm-4">
+				<input type="text" class="form-control" name="title">
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-4">
+				<button type="submit" class="btn btn-default">Send</button>
+			</div>
+		</div>
 	</form>
 </div>
